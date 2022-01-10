fbdd292deff0dbc95c007fe5ce32e61ed69f7c07
damsarabi <sam.darabi@gmail.com>
2022-01-10 12:09:01 -0800

    balls

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
       document.getElementById("fret-board").className = "container grid " + (document.getElementById("chord").value).toLowerCase()
    }
    changeChord();
-   
    document.getElementById("chord").addEventListener("change", changeChord);
 })();
 
