d1517d1095c75d4400429ffe3c64d2206a344d4d
damsarabi <sam.darabi@gmail.com>
2022-01-10 13:28:34 -0800

    changes..

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 (function() {
    function changeChord() {
+      document.getElementById("chord").value = document.getElementById("root").value + document.getElementById("flavor").value + document.getElementById("extention").value
       document.getElementById("fret-board").className = "container grid " + (document.getElementById("chord").value).toLowerCase()
    }
    changeChord();
    document.getElementById("chord").addEventListener("change", changeChord);
+   document.getElementById("root").addEventListener("change", changeChord);
+   document.getElementById("flavor").addEventListener("change", changeChord);
+   document.getElementById("extention").addEventListener("change", changeChord);
+
+   var el = document.querySelectorAll("#fret-board > div");
+
+   for (let i = 0; i < el.length; i++) {
+     el[i].addEventListener("click", function() {
+      document.getElementById("root").value = el[i].className;
+      changeChord();
+     });
+   }
+   
+
 })();
 
